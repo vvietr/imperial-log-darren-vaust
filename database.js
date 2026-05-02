4bb6c5cb9c52456ff2f20906b5414f00eaa8320b
The Shipwright <nicktheusername1017@gmail.com>
2026-05-02 17:00:44 -0400

    Update database.js

--- database.js
+++ database.js
@@ -29,13 +29,28 @@ const imperialLogs = [
         title: "Cartography Sucks",
         personnel: "",                 // Leave blank to auto-display "N/A"
         author: "",                    // For 'logs', blank auto-defaults to GC-3170
-        terminal: "Tablet",            // Seeded generator uses this to assign the specific terminal. Must be 'Command', 'Tablet', 'Barracks', or 'Field'.
+        terminal: "Command",            // Seeded generator uses this to assign the specific terminal. Must be 'Command', 'Tablet', 'Barracks', or 'Field'.
         imageExt: "jpg",               // You mentioned the map is a JPG. Upload it as: map_log_1_img.jpg
         body: `Based on helmet-recorded media and available imagery - along with word of mouth from the scouts and our more experienced troopers - I've compiled a crude overlay with a relatively recent orbital scan to create a map of the region surrounding the main base here on Felucia. My cartography skills are... limited, I'll admit that, but it should at least be better than "o' yea I've got it right 'er in me noggin!" from whomever we choose to lead an expedition.
 
 There are some interesting conclusions supported by this map, though: Drills 1 and 2 are most definitely the easiest to maintain. Drills 3 and 4, however, are quite entrenched in the forestry and, unfortunately, very near to known nesting locations for aggressive fauna. Oh well, I don't suppose there's much you can do but rely on the aim of your average trooper being true.`
     }
 
+    ,{
+        id: "bff_1",
+        tab: "logs",
+        date: "33.2.01 | ~07h",
+        title: "A Friendly Face In A Chaos Waste",
+        personnel: "SP-6167",
+        author: "",
+        terminal: "Barracks",
+        imageExt: "jpg",
+        body: `I haven't been here too long, but I've already been lucky enough to find a friendly face among the troopers - A sapper by the designation of 6170. We met back before I was hypershipped off to Lothal, in recruit processing. I didn't know much of him then... and I don't suppose I know much of him now, come to think of it. I ought to get to know him better - the more friends the merrier in a void-hell like this one.
+
+Seriously... in all its wisdom, the Imperial engineers couldn't come up with a series of pumps and pipes to put water in our purifiers? We've got fucking moisture vaporators for Marka's-sake! I'd expect to see those -maybe- as a backup, in the direst sense of the word, for a desert-based facility... but in a swamp? A region that is, by definition, saturated with water!
+
+Does a promotion to Imperial Engineering Officer come with a complimentary fucking cranial excavation or something?`
+    }
     // --- COPY AND PASTE THIS BLANK TEMPLATE FOR NEW ENTRIES ---
     /*
     ,{
